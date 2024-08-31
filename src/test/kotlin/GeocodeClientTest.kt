c60d5c80929007bd0a56ca88be0e9fee5fb39866
ni-xon <nixon.s.uch@gmail.com>
2024-09-01 00:18:26 +1000

    test: add GeocodeClient test

---
+++ src/test/kotlin/GeocodeClientTest.kt
@@ -0,0 +1,49 @@
+import clients.GeocodeClient
+import clients.GeocodeResponse
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+class GeocodeClientTest {
+
+    private val httpClient = mockk<HttpClient>()
+    private val subject = GeocodeClient(httpClient)
+
+    @Nested
+    inner class GetCoordinateBoundsByAddress {
+
+        @Test
+        fun `when address is found return bounds`() {
+            val mockResponse: HttpResponse<String> = mockk()
+            val mockResponseBody = this::class.java.getResource("/geocode-api-found-response.json")?.readText(Charsets.UTF_8)
+            every { mockResponse.body() } returns mockResponseBody
+            every { mockResponse.statusCode() } returns 200
+            every {
+                httpClient.send<String>(any<HttpRequest>(), any())
+            } returns mockResponse
+
+            val response = subject.getCoordinateBoundsByAddress("Mataranka")
+
+            assertEquals(-14.9230049, response.results.first().geometry.location.lat)
+            assertEquals( 133.0667202, response.results.first().geometry.location.lng)
+        }
+
+        @Test
+        fun `when address is not found return empty list of results`() {
+            val mockResponse: HttpResponse<String> = mockk()
+            val mockResponseBody = this::class.java.getResource("/geocode-api-not-found-response.json")?.readText(Charsets.UTF_8)
+            every { mockResponse.body() } returns mockResponseBody
+            every { mockResponse.statusCode() } returns 200
+            every {
+                httpClient.send<String>(any<HttpRequest>(), any())
+            } returns mockResponse
+
+            assertEquals(GeocodeResponse(emptyList()), subject.getCoordinateBoundsByAddress("Marka"))
+        }
+    }
+}
