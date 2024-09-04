5236e2b71724066304b31394737ad2cbfea1930b
ni-xon <nixon.s.uch@gmail.com>
2024-09-04 23:57:06 +1000

    refactor: move dependency intialisation to field

--- src/main/kotlin/com/nixonsu/cheephonk/ApplicationHandler.kt
+++ src/main/kotlin/com/nixonsu/cheephonk/ApplicationHandler.kt
@@ -13,18 +13,17 @@ import com.nixonsu.cheephonk.utils.makeNotificationMessage
 import java.net.http.HttpClient
 
 class ApplicationHandler : RequestHandler<Map<String, Any>, String> {
-    override fun handleRequest(input: Map<String, Any>, context: Context): String {
-        // Initialise dependencies
-        val httpClient = HttpClient.newHttpClient()
-        val objectMapper: ObjectMapper = ObjectMapper()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .registerModule(KotlinModule())
-        val petrolSpyClient = PetrolSpyClient(httpClient, objectMapper)
-        val googleGeocodingClient = GoogleGeocodingClient(httpClient, objectMapper)
-        val googleDistanceMatrixClient = GoogleDistanceMatrixClient(httpClient, objectMapper)
-        val fuelPriceService = FuelPriceService(petrolSpyClient, googleGeocodingClient, googleDistanceMatrixClient)
-        val telegramClient = TelegramClient(httpClient, objectMapper)
+    private val httpClient = HttpClient.newHttpClient()
+    private val objectMapper: ObjectMapper = ObjectMapper()
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .registerModule(KotlinModule())
+    private val petrolSpyClient = PetrolSpyClient(httpClient, objectMapper)
+    private val googleGeocodingClient = GoogleGeocodingClient(httpClient, objectMapper)
+    private val googleDistanceMatrixClient = GoogleDistanceMatrixClient(httpClient, objectMapper)
+    private val fuelPriceService = FuelPriceService(petrolSpyClient, googleGeocodingClient, googleDistanceMatrixClient)
+    private val telegramClient = TelegramClient(httpClient, objectMapper)
 
+    override fun handleRequest(input: Map<String, Any>, context: Context): String {
         val stations = fuelPriceService.getNCheapestStationsBySuburb(5, "Springvale")
 
         val message = makeNotificationMessage(stations)
@@ -33,56 +32,4 @@ class ApplicationHandler : RequestHandler<Map<String, Any>, String> {
 
         return "OK"
     }
-}
-
-class CustomContext : Context {
-    override fun getAwsRequestId(): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun getLogGroupName(): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun getLogStreamName(): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun getFunctionName(): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun getFunctionVersion(): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun getInvokedFunctionArn(): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun getIdentity(): CognitoIdentity {
-        TODO("Not yet implemented")
-    }
-
-    override fun getClientContext(): ClientContext {
-        TODO("Not yet implemented")
-    }
-
-    override fun getRemainingTimeInMillis(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun getMemoryLimitInMB(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun getLogger(): LambdaLogger {
-        TODO("Not yet implemented")
-    }
-
-}
-
-fun main() {
-    val applicationHandler = ApplicationHandler()
-    applicationHandler.handleRequest(emptyMap(), CustomContext())
 }
