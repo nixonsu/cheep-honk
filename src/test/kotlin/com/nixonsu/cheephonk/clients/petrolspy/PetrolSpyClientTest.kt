eff036250f8abfc5b4d325bff3bf48a1404b1510
ni-xon <nixon.s.uch@gmail.com>
2024-09-06 15:34:49 +1000

    Revert "chore: temp disable test"
    This reverts commit d656abfaeacb8d3e3bb5dd95abc86ff2a0f47532.

--- src/test/kotlin/com/nixonsu/cheephonk/clients/petrolspy/PetrolSpyClientTest.kt
+++ src/test/kotlin/com/nixonsu/cheephonk/clients/petrolspy/PetrolSpyClientTest.kt
@@ -10,7 +10,6 @@ import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.net.http.HttpClient
@@ -18,7 +17,6 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import kotlin.random.Random.Default.nextDouble
 
-@Disabled
 class PetrolSpyClientTest {
 
     private val httpClient = mockk<HttpClient>()
