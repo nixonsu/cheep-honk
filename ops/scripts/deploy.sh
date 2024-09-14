abb4f10cc01954fa0788a7f6a7a349ff4add894a
ni-xon <nixon.s.uch@gmail.com>
2024-09-14 12:30:13 +1000

    fix: surround variables in quotes

--- ops/scripts/deploy.sh
+++ ops/scripts/deploy.sh
@@ -6,11 +6,11 @@ aws cloudformation deploy \
 --template-file ./ops/deployment/packaged-template.yaml \
 --stack-name CheepHonkResources \
 --parameter-overrides \
-    GitSHA=${GITHUB_SHA:0:8} \
-    GoogleApiKey=${GOOGLE_API_KEY} \
-    ProxyApiKey=${PROXY_API_KEY} \
-    TelegramAuthToken=${TELEGRAM_AUTH_TOKEN} \
-    TelegramChatId=${TELEGRAM_CHAT_ID} \
-    OriginAddress=${ORIGIN_ADDRESS} \
+    GitSHA="${GITHUB_SHA:0:8}" \
+    GoogleApiKey="${GOOGLE_API_KEY}" \
+    ProxyApiKey="${PROXY_API_KEY}" \
+    TelegramAuthToken="${TELEGRAM_AUTH_TOKEN}" \
+    TelegramChatId="${TELEGRAM_CHAT_ID}" \
+    OriginAddress="${ORIGIN_ADDRESS}" \
 --capabilities CAPABILITY_IAM
 echo "Done!"
